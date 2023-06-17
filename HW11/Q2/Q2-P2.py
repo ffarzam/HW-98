0e26235aa8e01a9cd886664e0cf94d77980e5245
Farzam <ffarzam_1992@yahoo.com>
2023-06-17 12:49:54 +0330

    HW11 - Q2 - Part 2
    Calculation of GPA with argparse madule

---
+++ HW11/Q2/Q2-P2.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-g","--grades", nargs="*", type=float)
+parser.add_argument("-f","--float", default=2 , type=int)
+args = parser.parse_args()
+
+GPA = sum(args.grades)/len(args.grades)
+
+print("\033[0;31m"f"{GPA =: .{args.float}f}" "\033[0m")
