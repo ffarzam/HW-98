c9d58ec4af3e64416ebd7476b41368fe9e56e4db
Farzam <ffarzam_1992@yahoo.com>
2023-06-18 00:06:10 +0330

    HW11 - Q5 - Part 3
    Calculating size of files with specific extention in a directory

--- HW11/Q5/Q5.py
+++ HW11/Q5/Q5.py
@@ -17,17 +17,32 @@ def get_file_size(path):
         return os.path.getsize(path)
     return f"No file with that name"
 
+def get_size_extension(path,file_format):
+    size = 0
+    for i in os.scandir(path):
+        if os.path.isfile(i):
+            if i.name.endswith(f".{file_format}"):
+                size += os.path.getsize(i)
+        else:
+            size += get_size_extension(i,file_format)
+    return size
+
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-d",type=pathlib.Path)
 group.add_argument("-f",type = pathlib.Path)
+parser.add_argument("-F",type = str)
 args=parser.parse_args()
 
 
 if args.d:
 
-    final_size=get_dir_size(args.d)
-    print("\033[0;31m" f"Size: {(final_size / 1024)} KB" "\033[0m")
+    if args.F:
+        final_size = get_size_extension(args.d, args.F)
+        print("\033[0;31m" f"Size: {(final_size / 1024)} KB" "\033[0m")
+    else:
+        final_size = get_dir_size(args.d)
+        print("\033[0;31m" f"Size: {(final_size / 1024)} KB" "\033[0m")
 
 elif args.f:
     final_size = get_file_size(args.f)
@@ -35,5 +50,3 @@ elif args.f:
         print("\033[0;31m" f"{final_size} " "\033[0m")
     else:
         print("\033[0;31m" f"Size: {(final_size / 1024)} KB" "\033[0m")
-
-
