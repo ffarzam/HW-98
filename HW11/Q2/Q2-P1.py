d1e68bdc8e484fb70b400fd3d120643e01094ad4
Farzam <ffarzam_1992@yahoo.com>
2023-06-17 13:12:51 +0330

    HW11 - Q2 - Part 1
    Update: Adding validate_number and make_one_string to filter the inputs

--- HW11/Q2/Q2-P1.py
+++ HW11/Q2/Q2-P1.py
@@ -1,4 +1,13 @@
 import sys
+import re
+
+
+def make_one_string(lst):
+    return ' '.join(lst)
+
+def validate_numbers(string):
+    num_lst = re.findall(r'[0-9]+',string)
+    return num_lst
 
 def gpa(lst):
     return sum(lst) / len(lst)
@@ -8,11 +17,12 @@ def make_float(lst):
 
 def main():
     pre_lst = sys.argv[1:]
-    float_lst = make_float(pre_lst)
+    my_string= make_one_string(pre_lst)
+    numbers_list= list(validate_numbers(my_string))
+    float_lst = make_float(numbers_list)
     result = gpa(float_lst)
     return result
 
 if __name__ == '__main__':
     GPA = main()
     print("\033[0;31m" f"GPA: {GPA}" "\033[0m")
-
