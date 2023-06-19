ea75179d6f61bb1b9ed2505469af872f92ba5845
Farzam <ffarzam_1992@yahoo.com>
2023-06-19 19:35:33 +0330

    HW11 - Q4
    Make the code more readable and cleaner

--- HW11/Q4/Q4.py
+++ HW11/Q4/Q4.py
@@ -1,23 +1,22 @@
 import random as rnd
 import argparse
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument("-s","--start", required = True, type = int)
-parser.add_argument("-e","--end", required = True, type = int)
-parser.add_argument("-g","--guess", required = True, type = int)
+parser.add_argument("-s", "--start", required=True, type=int)
+parser.add_argument("-e", "--end", required=True, type=int)
+parser.add_argument("-g", "--guess", required=True, type=int)
 args = parser.parse_args()
 
-objective= rnd.randint(args.start, args.end)
+objective = rnd.randint(args.start, args.end)
 print(objective)
-count=1
+count = 1
 while True:
     print("\033[0;31m"f"Enter number #{count}""\033[0m")
-    num= int(input("\033[0;32m""Enter your number:""\033[0m"))
+    num = int(input("\033[0;32m""Enter your number:""\033[0m"))
     if num == objective:
         print("\033[0;34m""Congratulation, You Won. Your guess was correct""\033[0m")
         break
-    elif count==args.g:
+    elif count == args.g:
         print("\033[0;35m""Sorry, You lost. None of your guess was correct.""\033[0m")
         break
     else:
@@ -27,4 +26,4 @@ while True:
         elif num > objective:
             print("\33[0;36m" "Enter Lower Number" "\033[0;m")
 
-        count+=1
+        count += 1
