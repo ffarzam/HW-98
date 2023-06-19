51caa05b3381631c476d0db30f895f756a741d7b
Farzam <ffarzam_1992@yahoo.com>
2023-06-19 19:36:40 +0330

    HW11 - Q5
    Make the code more readable and cleaner

--- HW11/Q5/Q5.py
+++ HW11/Q5/Q5.py
@@ -9,30 +9,33 @@ def get_dir_size(path):
         if os.path.isfile(i):
             size += os.path.getsize(i)
         else:
-            size+=get_dir_size(i)
+            size += get_dir_size(i)
     return size
+
 
 def get_file_size(path):
     if os.path.exists(path):
         return os.path.getsize(path)
     return f"No file with that name"
 
-def get_size_extension(path,file_format):
+
+def get_size_extension(path, file_format):
     size = 0
     for i in os.scandir(path):
         if os.path.isfile(i):
             if i.name.endswith(f".{file_format}"):
                 size += os.path.getsize(i)
         else:
-            size += get_size_extension(i,file_format)
+            size += get_size_extension(i, file_format)
     return size
+
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument("-d",type = pathlib.Path)
-group.add_argument("-f",type = pathlib.Path)
-parser.add_argument("-F",type = str)
-args=parser.parse_args()
+group.add_argument("-d", type=pathlib.Path)
+group.add_argument("-f", type=pathlib.Path)
+parser.add_argument("-F", type=str)
+args = parser.parse_args()
 
 if args.d:
     if args.d.is_dir():
@@ -48,7 +51,7 @@ if args.d:
 elif args.f:
     if args.f.is_file():
         final_size = get_file_size(args.f)
-        if isinstance(final_size,str):
+        if isinstance(final_size, str):
             print("\033[0;31m" f"{final_size} " "\033[0m")
         else:
             print("\033[0;31m" f"Size: {(final_size / 1024)} KB" "\033[0m")
