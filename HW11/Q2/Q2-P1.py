804a08b8e1f022796489b626cb0ecf71fcdafa51
Farzam <ffarzam_1992@yahoo.com>
2023-06-19 19:31:16 +0330

    HW11 - Q2 - Part 1
    Make the code more readable and cleaner

--- HW11/Q2/Q2-P1.py
+++ HW11/Q2/Q2-P1.py
@@ -5,23 +5,28 @@ import re
 def make_one_string(lst):
     return ' '.join(lst)
 
+
 def validate_numbers(string):
-    num_lst = re.findall(r'[0-9]+',string)
+    num_lst = re.findall(r'[0-9]+', string)
     return num_lst
+
 
 def gpa(lst):
     return sum(lst) / len(lst)
 
+
 def make_float(lst):
-    return list(map(float,lst))
+    return list(map(float, lst))
+
 
 def main():
     pre_lst = sys.argv[1:]
-    my_string= make_one_string(pre_lst)
-    numbers_list= list(validate_numbers(my_string))
+    my_string = make_one_string(pre_lst)
+    numbers_list = list(validate_numbers(my_string))
     float_lst = make_float(numbers_list)
     result = gpa(float_lst)
     return result
+
 
 if __name__ == '__main__':
     GPA = main()
