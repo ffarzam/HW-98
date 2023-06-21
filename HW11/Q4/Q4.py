7cd5f1e70f4c0d948f31cc91ee93d7b58d8eccdd
Farzam <ffarzam_1992@yahoo.com>
2023-06-21 10:51:19 +0330

    HW11 - Q4
    Using random API instead random module

--- HW11/Q4/Q4.py
+++ HW11/Q4/Q4.py
@@ -1,5 +1,6 @@
 import random as rnd
 import argparse
+import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--start", required=True, type=int)
@@ -7,8 +8,11 @@ parser.add_argument("-e", "--end", required=True, type=int)
 parser.add_argument("-g", "--guess", required=True, type=int)
 args = parser.parse_args()
 
-objective = rnd.randint(args.start, args.end)
-print(objective)
+url = f"https://www.random.org/integers/?num=1&min={args.start}&max={args.end}&col=1&base=10&format=plain&rnd=new"
+response = requests.get(url)
+print(objective := int(response.text))
+# objective = rnd.randint(args.start, args.end)
+# print(objective)
 count = 1
 while True:
     print("\033[0;31m"f"Enter number #{count}""\033[0m")
@@ -16,14 +20,13 @@ while True:
     if num == objective:
         print("\033[0;34m""Congratulation, You Won. Your guess was correct""\033[0m")
         break
-    elif count == args.g:
+    elif count == args.guess:
         print("\033[0;35m""Sorry, You lost. None of your guess was correct.""\033[0m")
         break
     else:
-        print("\033[0;36m" f"Your guess was incorrect. You have {args.g - count} more guess""\033[0m")
+        print("\033[0;36m" f"Your guess was incorrect. You have {args.guess - count} more guess""\033[0m")
         if num < objective:
             print("\33[0;36m" "Enter Higher Number" "\033[0;m")
         elif num > objective:
             print("\33[0;36m" "Enter Lower Number" "\033[0;m")
-
         count += 1
