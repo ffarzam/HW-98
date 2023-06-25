40b1340d2aabb90718f46bcc40912d170ffca0b5
Farzam <ffarzam_1992@yahoo.com>
2023-06-25 17:55:04 +0330

    HW12 - database.py - Update
    Adding create_table method

--- HW12/database.py
+++ HW12/database.py
@@ -5,3 +5,19 @@ class WeatherDatabase:
     def __init__(self, dbname="weather", user="postgres", password="ffarzam_1992", host='localhost', port='5432'):
         self.conn = psycopg2.connect(database=dbname, user=user, password=password, host=host, port=port)
         self.cur = self.conn.cursor()
+
+    def create_tables(self):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS Requests(
+                            id BIGSERIAL PRIMARY KEY NOT NULL,
+                            city VARCHAR(50) NOT NULL,
+                            request_time TIMESTAMP);""")
+
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS Responses(
+                            id BIGSERIAL PRIMARY KEY NOT NULL,
+                            request_id BIGINT NOT NULL UNIQUE,
+                            city VARCHAR(50) NOT NULL,
+                            temperature DECIMAL NOT NULL,
+                            feels_like_temperature DECIMAL NOT NULL,
+                            last_updated_time TIMESTAMP NOT NULL,
+                            FOREIGN KEY (request_id) REFERENCES Requests(id));""")
+        self.conn.commit()
