cfa157aac7c1ef43d7706b28f148b77876e3f89b
Farzam <ffarzam_1992@yahoo.com>
2023-06-25 16:35:31 +0330

    HW12 - weather_server.py - Update
    Adding get_city_weather function

--- HW12/weather_server.py
+++ HW12/weather_server.py
@@ -1,4 +1,5 @@
 import requests as rq
+from datetime import datetime
 
 
 class ConnectionManager:
@@ -11,3 +12,18 @@ class ConnectionManager:
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.file.close()
+
+
+def get_city_weather(city: str) -> dict:
+    weather_info = {}
+    RAW_URL = "http://api.openweathermap.org/data/2.5/weather"
+    API_KEY = "3eb2381ac662acb4defa744088a680ba"
+    final_url = RAW_URL + "?q=" + city + "&appid=" + API_KEY + "&units=metric"
+
+    with ConnectionManager(final_url) as response:
+        if response:
+            weather_info["temperature"] = response.json()["main"]["temp"]
+            weather_info["feels_like"] = response.json()["main"]["feels_like"]
+            weather_info["last_updated"] = datetime.fromtimestamp(response.json()["dt"]).strftime("%Y-%m-%d %H:%M:%S")
+            return weather_info
+    return response.json()
