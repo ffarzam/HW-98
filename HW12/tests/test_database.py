de6e9cac2ea0614a89181dd1969195163e8d0351
Farzam <ffarzam_1992@yahoo.com>
2023-06-26 08:58:41 +0330

    HW12 - test_database.py - Update
    Adding test_save_request_data function

--- HW12/tests/test_database.py
+++ HW12/tests/test_database.py
@@ -1,5 +1,6 @@
 import pytest
 from HW12.database import WeatherDatabase
+import datetime
 
 
 @pytest.fixture
@@ -21,3 +22,22 @@ def test_create_tables(db):
 
     assert 'requests' in tables
     assert 'responses' in tables
+
+
+def test_save_request_data(db):
+    city_name = "rasht"
+    request_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    db.save_request_data(city_name, request_time)
+
+    cursor = db.conn.cursor()
+    cursor.execute("SELECT * FROM requests ORDER BY id DESC")
+    row = cursor.fetchone()
+
+    assert row[1] == city_name
+    assert row[2] == datetime.datetime.strptime(request_time, "%Y-%m-%d %H:%M:%S")
+    cursor.execute('''SELECT last_value FROM requests_id_seq''')
+    id_num = cursor.fetchone()[0]
+    cursor.execute(f'''DELETE FROM requests WHERE id = {id_num};''')
+    cursor.execute(f'''SELECT setval('requests_id_seq', {id_num}, false);''')
+    db.conn.commit()
