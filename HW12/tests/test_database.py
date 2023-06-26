734360c492a6020d742b524bd05c4c28cd414df8
Farzam <ffarzam_1992@yahoo.com>
2023-06-26 08:57:23 +0330

    HW12 - test_database.py - Update
    Adding test_create_tables function

--- HW12/tests/test_database.py
+++ HW12/tests/test_database.py
@@ -7,3 +7,17 @@ def db():
     db = WeatherDatabase()
     yield db
     db.close()
+
+
+def test_create_tables(db):
+    db.create_tables()
+    cursor = db.cur
+
+    cursor.execute('''SELECT table_name 
+                        FROM information_schema.tables 
+                        WHERE table_schema = 'public' AND table_type = 'BASE TABLE';''')
+
+    tables = [table[0] for table in cursor.fetchall()]
+
+    assert 'requests' in tables
+    assert 'responses' in tables
