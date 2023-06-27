b71be3a2b4084f75ef7a50a6676ca09537323f79
Farzam <ffarzam_1992@yahoo.com>
2023-06-27 21:34:55 +0330

    HW12 - users.py - Add
    Adding user class

---
+++ HW12/users.py
@@ -0,0 +1,31 @@
+from validators import *
+import requests as rq
+
+
+class User:
+    username = Username_UDescriptor()
+    password = Password_UDescriptor()
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    @classmethod
+    def register(cls, username, password):
+        return cls(username, password)
+
+    def save(self):
+        response = rq.post("http://192.168.1.167:8080/",
+                           json={"action": "signin", "username": self.username, "password": self.password})
+
+    @classmethod
+    def login(cls, username, password):
+        response = rq.post("http://192.168.1.167:8080/",
+                           json={"action": "login", "username": username, "password": password})
+
+        return response.json()["status"]
+
+    @staticmethod
+    def get_city(username, city_name):
+        response = rq.get(f"http://192.168.1.167:8080/?user={username}&city={city_name}")
+        return response
