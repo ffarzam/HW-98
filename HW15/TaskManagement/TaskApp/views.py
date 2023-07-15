3b99ce5aeca6ab3094e518afbe61c9fe6288edb6
Farzam <ffarzam_1992@yahoo.com>
2023-07-15 17:13:12 +0330

    Updating TaskApp views
    Adding search feature

--- HW15/TaskManagement/TaskApp/views.py
+++ HW15/TaskManagement/TaskApp/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Task, Tag
 from django.http import Http404
-
-
-# from django.http import Http404
+from django.db.models import Q
+from itertools import chain
 
 
 # Create your views here.
@@ -14,12 +13,6 @@ def home(request):
 
 def tasks(request):
     all_tasks = Task.objects.all()
-
-    # print(all_tasks)
-    for task in list(all_tasks):
-        print(type(task.tag))
-        print(task.tag)
-
     context = {"tasks": all_tasks}
     return render(request, "tasks.html", context=context)
 
@@ -31,3 +24,25 @@ def task_details(request, pk):
         return render(request, "task_details.html", context=context)
     except:
         raise Http404("No matches the given query.")
+
+
+def search(request):
+    return render(request, 'search.html')
+
+
+def search_result(request):
+    if request.method == "GET":
+        # results = request.POST['searched']
+        searched = request.GET.get('searched')
+
+        results = list(chain(Task.objects.filter(title__icontains=searched),
+                             Task.objects.filter(description__icontains=searched),
+                             Task.objects.filter(tag__name__icontains=searched)
+                             )
+                       )
+        # results = Task.objects.filter(Q(title__icontains=searched)
+        #                               | Q(description__icontains=searched)
+        #                               | Q(tag__name__icontains=searched)
+        #                               )
+
+    return render(request, 'search_result.html', {"searched": searched, "results": results})
