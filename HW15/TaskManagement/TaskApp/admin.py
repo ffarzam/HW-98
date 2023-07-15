dc3c3b4f424c1236f37d823a61f5c9250b13ee36
Farzam <ffarzam_1992@yahoo.com>
2023-07-15 12:31:21 +0330

    Adding models to admin panel

--- HW15/TaskManagement/TaskApp/admin.py
+++ HW15/TaskManagement/TaskApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Task, Tag
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(Tag)
