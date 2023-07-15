ac862dc8f639ab43ccb7b7fdc8571b735b8bb343
Farzam <ffarzam_1992@yahoo.com>
2023-07-15 17:11:37 +0330

    Updating TaskApp urls

--- HW15/TaskManagement/TaskApp/urls.py
+++ HW15/TaskManagement/TaskApp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('tasks/', views.tasks, name="tasks"),
     path('tasks/<int:pk>/', views.task_details, name="task_details"),
+    path('search/', views.search, name='search'),
+    path('search_result/', views.search_result, name='search_result'),
 ]
