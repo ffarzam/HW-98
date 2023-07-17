b5e664bd4201168b04386c31823a68212cf0a91d
Farzam <ffarzam_1992@yahoo.com>
2023-07-17 19:48:35 +0330

    Updating setting.py

--- HW15/TaskManagement/TaskManagement/settings.py
+++ HW15/TaskManagement/TaskManagement/settings.py
@@ -116,7 +116,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     BASE_DIR / "static/css",
     BASE_DIR / "static/favicon",
-]
+    BASE_DIR / "static/logo",
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
